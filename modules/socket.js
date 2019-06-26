bf55841d0031995544e25d01dd7d479b07c18c2b
Jackz <me@jackz.me>
2019-06-25 22:41:24 -0500

    added blends size, date

--- modules/socket.js
+++ modules/socket.js
@@ -9,6 +9,7 @@ const prettyMilliseconds = require('pretty-ms');
 
 const UPDATE_INTERVAL = 1000*(process.env.STAT_UPDATE_INTERVAL_SECONDS||30);
 const ZIP_DIR = process.env.ZIP_DIR||`${process.env.HOME_DIR}/zips`
+const BLENDS_DIR = process.env.BLENDS_DIR||`${process.env.HOME_DIR}/blends`
 
 const {version} = require('../package.json')
 let render = {
@@ -53,9 +54,30 @@ function main() {
         socket.on('blends',async(data,callback) => {
             //return callback({files:[{name:'test.blend'}]})
             try {
-                const files_raw = await fs.readdir(`${process.env.HOME_DIR}/blends`);
-                const files = files_raw.filter(v => v.endsWith(".blend")).map(v => {return {name:v}});
-                callback({files})
+                const files_raw = await fs.readdir(BLENDS_DIR);
+                const promises = [];
+                files_raw.forEach(v => {
+                    if(!v.endsWith(".blend")) return;
+                    const promise = new Promise((resolve,reject) => {
+                        fs.stat(`${BLENDS_DIR}/${v}`)
+                        .then(stat => {
+                            resolve({
+                                name:v,
+                                size:stat.size,
+                                date:prettyMilliseconds(stat.mtimeMs)
+                            })
+                        }).catch(err => reject(err));
+                    })
+                    promises.push(promise);
+                })
+
+                Promise.all(promises)
+                .then(files => {
+                    callback({files})
+                }).catch((err) => {
+                    callback({error:err.message})
+                    console.error('[Error]',err.message)
+                })
             }catch(err) {
                 console.log(err)
                 callback({error:err.message})
