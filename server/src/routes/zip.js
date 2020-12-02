bb37fa9209f4f41ef25322fae61dd36f025bcab4
Jackz <me@jackz.me>
2020-12-01 22:37:28 -0600

    Implement token-zip downloading

--- server/src/routes/zip.js
+++ server/src/routes/zip.js
@@ -10,26 +10,55 @@ const { restrictedCheck, userCheck } = require('../modules/Middlewares');
 const ZIP_DIR = process.env.ZIP_DIR || `${process.env.HOME_DIR}/zips`
 const BLENDS_DIR = process.env.BLENDS_DIR || `${process.env.HOME_DIR}/blends`
 
+const DL_TOKENS = new Map(); //<filename,{token:<expires>}>
+
 router.use(fileUpload({
     limits: { fileSize: 500 * 1024 * 1024 }, //MB
     abortOnLimit: true,
 }));
 
+router.get('/debug', (req,res) => {
+    res.json(DL_TOKENS)
+})
 
-router.get('/:name', userCheck, (req,res) => {
-    res.set('Content-Type', 'application/zip')
-    res.set('Content-Disposition', `attachment; filename="${req.params.name}"`);
+router.get('/:name/download', (req,res) => {
+    if(req.query.token) {
+        const downloadTokens = DL_TOKENS.get(req.params.name);
+        if(downloadTokens && downloadTokens[req.query.token] && downloadTokens[req.query.token] >= Date.now()) {
+            res.set('Content-Type', 'application/zip')
+            res.set('Content-Disposition', `attachment; filename="${req.params.name}"`);
 
-    const stream = createReadStream(`${ZIP_DIR}/${req.params.name}`)
-    .on('open',() => {
-        stream.pipe(res)
-    })
-    .on('error', (err) => {
-        res.status(500).send(err)
-    })
-    .on('end', () => {
-        res.end();
-    })
+            const stream = createReadStream(`${ZIP_DIR}/${req.params.name}`)
+            .on('open',() => {
+                stream.pipe(res)
+            })
+            .on('error', (err) => {
+                res.status(500).send(err)
+            })
+            .on('end', () => {
+                res.end();
+            })
+        }else{
+            return res.status(403).json({error: 'No download exists for that zip file or token has expired. Request one with POST /zips/:name/token.', code:'INVALID_TOKEN'})
+        }
+    }else{
+        return res.status(403).json({error: 'Missing download token query parameter. Request one with POST /zips/:name/token.', code: 'MSSING_TOKEN'})
+    }
+})
+router.post('/:name/token', userCheck, async(req,res) => {
+    try {
+        await fs.stat(`${ZIP_DIR}/${req.params.name}`)
+        const token = generateUID();
+        const expires = new Date();
+        expires.setDate(expires.getHours() + 4)
+
+        let tokens = DL_TOKENS.has(req.params.name) || {};
+        tokens[token] = expires.getTime();
+        DL_TOKENS.set(req.params.name, tokens)
+        return res.json({token})
+    }catch(err) {
+        return res.status(404).json({error: "That zip does not exist."})
+    }
 })
 router.delete('/:name', userCheck, (req,res) => {
     fs.unlink(`${ZIP_DIR}/${req.params.name}`)
@@ -90,3 +119,29 @@ router.post('/upload', userCheck, (req,res) => {
     }
 })
 module.exports = router;
+
+function generateUID() {
+    // I generate the UID from two parts here 
+    // to ensure the random number provide enough bits.
+    var firstPart = (Math.random() * 46656) | 0;
+    var secondPart = (Math.random() * 46656) | 0;
+    firstPart = ("000" + firstPart.toString(36)).slice(-3);
+    secondPart = ("000" + secondPart.toString(36)).slice(-3);
+    return firstPart + secondPart;
+}
+
+setInterval(() => {
+    const now = Date.now()
+    DL_TOKENS.forEach((tokens, file) => {
+        for(const token in tokens) {
+            if(now >= tokens[token]) {
+                delete tokens[token];
+            }
+        }
+        if(Object.keys(tokens[token]).length == 0) {
+            DL_TOKENS.delete(file)
+        }else{
+            DL_TOKENS.set(file, tokens)
+        }
+    })
+}, 1000 * 60 * 30) //run every 30 minutes, clear up any expired tokens
