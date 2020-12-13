f28736c312d2b2b4a9c23864221f242d46d1a7b6
Jackz <me@jackz.me>
2020-12-12 20:04:43 -0600

    Minor UI tweaks

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -8,3 +8,13 @@
 export default {
 }
 </script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  
+}
+</style>
--- ui/src/views/ServerPage.vue
+++ ui/src/views/ServerPage.vue
@@ -661,13 +661,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  
-}
+
 .disconnected {
     background-color: lightgray !important;
 }
