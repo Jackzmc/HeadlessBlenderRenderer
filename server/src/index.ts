96c2c9291607462e82e4e383563221fd1a0d7493
Jackz <jacksonbixby@gmail.com>
2023-02-09 10:39:19 -0600

    Wait for render to stop on shutdown

--- server/src/index.ts
+++ server/src/index.ts
@@ -36,6 +36,17 @@ import WebServer from './server'
     async function gracefulShutdown() {
         console.info('Received shutdown signal. Cancelling any active renders...')
         await renderController.cancelRender("SHUTTING_DOWN")
+        if(renderController.active) {
+            console.info("Waiting for render to stop")
+            // Poll until render_stops
+            let timeout = 30_000 //30s
+            while(renderController.active || timeout > 0) {
+                await sleep(1000)
+                timeout -= 1000
+            }
+        }
         process.exit(0)
     }
 })()
+
+const sleep = time => new Promise(res => setTimeout(res, time));
