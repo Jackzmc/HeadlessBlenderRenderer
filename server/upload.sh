84c186a123f8c10abb2f68357019d0894e3e736a
Jackz <jacksonbixby@gmail.com>
2023-02-09 11:56:16 -0600

    Fix deploy

--- server/upload.sh
+++ server/upload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 tsc && \
-    scp -r package.json yarn.lock dist/* jackz@192.168.1.15:/home/jackz/HeadlessBlenderRenderer-Production/server/dist/ && \
+    scp -r package.json yarn.lock jackz@192.168.1.15:/home/jackz/HeadlessBlenderRenderer-Production/server/ && \
+    scp -r dist/* jackz@192.168.1.15:/home/jackz/HeadlessBlenderRenderer-Production/server/dist/ && \
     yarn
